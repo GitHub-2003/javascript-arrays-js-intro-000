15628b0e48c7b5dd29c686c0bd0cb8d09b1991aa
MJU <marie.ju2012@outlook.com>
2020-03-10 15:02:38 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,47 @@
+// add to beginning; return a new array and not modify the original
+function addElementToBeginningOfArray(array, element) {
+  var newArray = new Array()
+  newArray = [element, ...array]
+  return array
+}
+// add to beginning; alter the original array that's passed in
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+// add to end; return a new array and not modify the original
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+// add to end; return a new array and not modify the original
+function destructivelyAddElementToEndOfArray (array, element) {
+  var newArray = new Array()
+  return [...newArray, element]
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+// return the entire mutated array after the 1st element is removed
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
+
+// return not mutated array
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+// remove last element; return the entire array mutated.
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+// remove last element; return the entire array NOT mutated.
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array, -1)
+  return array
+}
